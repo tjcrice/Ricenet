0ae91f17da60effda8e72f62027a19f487d9b093
Toby Rice <tobyjohnrice@gmail.com>
2023-06-10 23:51:00 +0100

    Add some registration logic

--- controllers/UsersController.cs
+++ controllers/UsersController.cs
@@ -9,6 +9,17 @@ public class UsersController : ControllerBase
     [HttpPost("register")]
     public async Task<IActionResult> Register(string username, string password)
     {
-        // I'll put registration logic here later
+        if (string.IsNullOrWhiteSpace(username) || string.IsNullOrWhiteSpace(password))
+        {
+            return BadRequest("Username and password are required.");
+        }
+
+        if (password.Length < 6)
+        {
+            return BadRequest("Password must be at least 6 characters long.");
+        }
+
+        // More registration logic will go here
+
     }
 }
